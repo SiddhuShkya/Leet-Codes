51e6585046337fc33696b76bfb45ef4b03271a99
SiddhuShkya <siddhuushakyaa@gmail.com>
2024-11-05 09:01:37 +0545

    Added a medium problem - Minimum Number of Changes to Make Binary String Beautiful

---
+++ Medium/Minimum Number of Changes to Make Binary String Beautiful/main.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minChanges(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            if s[l] != s[r]:
+                if r & 1:
+                    res += 1
+                l = r
+        return res
+                    
+        
