e54e7155bf53e95db63351fcd70a4d009d1cd02d
SiddhuShkya <siddhuushakyaa@gmail.com>
2024-12-17 09:06:37 +0545

    Added an easy problem - Count Pairs Whose Sum is Less than Target

---
+++ Easy/Count Pairs Whose Sum is Less than Target/main.py
@@ -0,0 +1,10 @@
+def countPairs(nums, target):
+    res = 0
+    nums.sort()  
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):  
+            if nums[i] + nums[j] < target:
+                res += 1
+    return res
+
+print(countPairs([-1, 1, 2, 3, 1], 2))
