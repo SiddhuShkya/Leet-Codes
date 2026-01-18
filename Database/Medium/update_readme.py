b9cd2b53298c60ebb3426fa9037860fc08724ec0
Siddhartha Shakya <siddhuushakyaa@gmail.com>
2026-01-18 10:28:51 +0545

    Updated the update_redme.py script

--- Database/Medium/update_readme.py
+++ Database/Medium/update_readme.py
@@ -74,6 +74,8 @@ This readme file contains solved SQL problems from LeetCode,from the Medium cate
 
 Each problem includes:
 
+- ✅ Table Schema
+- ✅ Problem Description
 - ✅ Input example
 - ✅ Correct SQL solution
 - ✅ Output example
